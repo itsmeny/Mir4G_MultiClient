7f5e4478d5d034529695c6c3500a4371a4765a40
itsmeny <chokjaroen.estk@mail.kmutt.ac.th>
2021-09-06 21:06:27 +0700

    src

---
+++ Mir4G_MultiClient/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Mir4G_MultiClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.refresh_btn = new System.Windows.Forms.Button();
+            this.processView = new System.Windows.Forms.ListView();
+            this.pid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.win_title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.file_path = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.file_arguments = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.เรยกToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ปดToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // refresh_btn
+            // 
+            this.refresh_btn.Location = new System.Drawing.Point(938, 12);
+            this.refresh_btn.Name = "refresh_btn";
+            this.refresh_btn.Size = new System.Drawing.Size(75, 23);
+            this.refresh_btn.TabIndex = 1;
+            this.refresh_btn.Text = "+ รีเฟรช";
+            this.refresh_btn.UseVisualStyleBackColor = true;
+            this.refresh_btn.Click += new System.EventHandler(this.refresh_btn_Click);
+            // 
+            // processView
+            // 
+            this.processView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.pid,
+            this.win_title,
+            this.file_arguments,
+            this.file_path});
+            this.processView.ContextMenuStrip = this.menu;
+            this.processView.FullRowSelect = true;
+            this.processView.GridLines = true;
+            this.processView.HideSelection = false;
+            this.processView.Location = new System.Drawing.Point(12, 41);
+            this.processView.MultiSelect = false;
+            this.processView.Name = "processView";
+            this.processView.Size = new System.Drawing.Size(1001, 245);
+            this.processView.TabIndex = 2;
+            this.processView.UseCompatibleStateImageBehavior = false;
+            this.processView.View = System.Windows.Forms.View.Details;
+            // 
+            // pid
+            // 
+            this.pid.Text = "Process Id";
+            this.pid.Width = 100;
+            // 
+            // win_title
+            // 
+            this.win_title.Text = "Window Title";
+            this.win_title.Width = 90;
+            // 
+            // file_path
+            // 
+            this.file_path.Text = "Arguments";
+            this.file_path.Width = 400;
+            // 
+            // file_arguments
+            // 
+            this.file_arguments.Text = "File Path";
+            this.file_arguments.Width = 400;
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.เรยกToolStripMenuItem,
+            this.ปดToolStripMenuItem});
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(134, 48);
+            this.menu.Opening += new System.ComponentModel.CancelEventHandler(this.menu_Opening);
+            // 
+            // เรยกToolStripMenuItem
+            // 
+            this.เรยกToolStripMenuItem.Name = "เรยกToolStripMenuItem";
+            this.เรยกToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.เรยกToolStripMenuItem.Text = "เรียกหน้าจอนี้";
+            this.เรยกToolStripMenuItem.Click += new System.EventHandler(this.เรยกToolStripMenuItem_Click);
+            // 
+            // ปดToolStripMenuItem
+            // 
+            this.ปดToolStripMenuItem.Name = "ปดToolStripMenuItem";
+            this.ปดToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ปดToolStripMenuItem.Text = "ปิดหน้าจอนี้";
+            this.ปดToolStripMenuItem.Click += new System.EventHandler(this.ปดToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 298);
+            this.Controls.Add(this.processView);
+            this.Controls.Add(this.refresh_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mir4G MultiClient";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button refresh_btn;
+        private System.Windows.Forms.ListView processView;
+        private System.Windows.Forms.ColumnHeader pid;
+        private System.Windows.Forms.ColumnHeader win_title;
+        private System.Windows.Forms.ColumnHeader file_path;
+        private System.Windows.Forms.ColumnHeader file_arguments;
+        private System.Windows.Forms.ContextMenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem เรยกToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ปดToolStripMenuItem;
+    }
+}
+
---
+++ Mir4G_MultiClient/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Management;
+namespace Mir4G_MultiClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void refresh_btn_Click(object sender, EventArgs e)
+        {
+            Process[] pMir4G = Process.GetProcessesByName("Mir4G");
+            processView.Items.Clear();
+            foreach (var p in pMir4G)
+            {
+                Console.WriteLine(p.Id + " " + p.MainWindowTitle);
+                string[] pArguments = GetCommandLine(p.Id).Split('\"');
+                ListViewItem pList = new ListViewItem(p.Id.ToString());
+                pList.SubItems.Add(p.MainWindowTitle);
+                pList.SubItems.Add(GetExecutablePath(p.Id));
+                pList.SubItems.Add(pArguments[2]);
+                processView.Items.Add(pList);
+            }
+
+        }
+
+        private string GetExecutablePath(int processId)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT ExecutablePath FROM Win32_Process WHERE ProcessId = " + processId))
+            using (ManagementObjectCollection objects = searcher.Get())
+            {
+                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["ExecutablePath"]?.ToString();
+            }
+        }
+        private string GetCommandLine(int processId)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + processId))
+            using (ManagementObjectCollection objects = searcher.Get())
+            {
+                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+            }
+        }
+
+        private void ปดToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void เรยกToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetForegroundWindow(Process.GetProcessById(int.Parse(processView.SelectedItems[0].Text)).MainWindowHandle);
+        }
+
+        private void menu_Opening(object sender, CancelEventArgs e)
+        {
+            if (processView.SelectedItems.Count > 0)
+            {
+                menu.Items[0].Enabled = true;
+                menu.Items[1].Enabled = true;
+            }
+            else
+            {
+                menu.Items[0].Enabled = false;
+                menu.Items[1].Enabled = false;
+            }
+        }
+    }
+}
